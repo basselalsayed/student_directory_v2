cd4d0d94452468615d8734119dbb1846618059e6
basselalsayed <alsayed.bassel@gmail.com>
2020-01-05 14:26:06 +0000

    improved rspec

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -2,9 +2,10 @@ require 'database'
 
 describe DataBase do 
   let(:subject) { described_class.new }
-  let(:student) { double(['Tony', 'December']) }
-  let(:student2) { double(['Ferguson', 'January']) }
+  let(:student) { double(:student, name: 'Tony', cohort: 'December') }
+  let(:student2) { double(:student2, name: 'Ferguson', cohort: 'January') }
   let(:expected_output) { ("1. Tony (December cohort)") }
+  let(:expected_output_2) { ("2. Ferguson (January cohort)")}
 
   
   describe '#initialization' do
@@ -18,21 +19,16 @@ describe DataBase do
 
   describe '#add_student' do
     before do
-      allow(subject).to receive(:add_student).with("Tony", :december).and_return(subject.students << student)
+      allow(subject).to receive(:add_student).with("tony", 'december').and_return(subject.students << student)
     end
     it 'adds a student to the array' do
-      subject.add_student('Tony', :december)
+      subject.add_student('tony', 'december')
       expect(subject.students).to include student
     end
   end
 
   describe '#print' do 
     before do 
-      
-      allow(student).to receive(:name).and_return("Tony")
-      allow(student2).to receive(:name).and_return("Ferguson")
-      allow(student).to receive(:cohort).and_return("December")
-      allow(student2).to receive(:cohort).and_return("January")
       allow(subject).to receive(:add_student).with("tony", 'december').and_return(subject.students << student)
       allow(subject).to receive(:add_student).with('ferguson', 'january').and_return(subject.students << student2)
       subject.add_student('tony', 'december')
@@ -50,10 +46,11 @@ describe DataBase do
     it 'raises an error when no matching name' do 
       expect{ subject.print_by_name('g') }.to raise_error "No matches found"
     end
-    # it '#print_all' do 
-    #   subject.print_all
-    #   expect{ subject.print_all }.to output(/#{Regexp.quote(expected_output)}/).to_stdout
-    # end
+    it '#print_all' do 
+      subject.print_all
+      expect{ subject.print_all }.to output(/#{Regexp.quote(expected_output)}/).to_stdout
+      expect{ subject.print_all }.to output(/#{Regexp.quote(expected_output_2)}/).to_stdout
+    end
   end
 
 end
